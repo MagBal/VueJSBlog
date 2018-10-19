26cf7b0ae6b6067fe644479643a430eca45daf39
Magda Balun <magda.balun@gmail.com>
2018-10-19 09:56:46 +0200

    Add Custom Directive to showBlog.vue file

--- src/main.js
+++ src/main.js
@@ -5,6 +5,27 @@ import VueResource from 'vue-resource'
 //uzycie plugina vue-resource
 Vue.use(VueResource);
 
+//custom directives
+Vue.directive('rainbow', {
+    bind(el, binding, vnode) {
+        el.style.color = "#" + Math.random().toString().slice(2, 8);
+    }
+});
+
+Vue.directive('theme', {
+    bind(el, binding, vnode) {
+        if (binding.value == "wide") {
+            el.style.maxWidth = "1200px";
+        } else if (binding.value == "narrow") {
+            el.style.maxWidth = "560px";
+        }
+        if (binding.arg == "column") { //w pliku showVue dyrektywa v-theme:column
+            el.style.padding = "20px";
+            el.style.background = "#ddd";
+        }
+    }
+})
+
 new Vue({
     el: '#app',
     render: h => h(App)
